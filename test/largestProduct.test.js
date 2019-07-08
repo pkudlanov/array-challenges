3687d50ecdc8a6ba5f90f0a66d859b7ffd4015db
Pavel Kudlanov <pkudlanov@yahoo.com>
2019-07-08 16:46:41 -0700

    number test passes

--- test/largestProduct.test.js
+++ test/largestProduct.test.js
@@ -2,8 +2,8 @@ const { largestProduct } = require('../lib/largestProduct');
 
 describe('test largest product function', () => {
     it('returns a number', () => {
-        const num = 14;
-        const result = largestProduct(num);
+        const arr = [14, 213, 124];
+        const result = largestProduct(arr);
         expect(result).toEqual(expect.any(Number));
     });
 });

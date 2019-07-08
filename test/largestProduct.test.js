6e5755bb53c9a38c2507ce3d3b19626f8b6117f9
Pavel Kudlanov <pkudlanov@yahoo.com>
2019-07-08 16:54:19 -0700

    wrote test if only number to return it

--- test/largestProduct.test.js
+++ test/largestProduct.test.js
@@ -6,4 +6,16 @@ describe('test largest product function', () => {
         const result = largestProduct(arr);
         expect(result).toEqual(expect.any(Number));
     });
+
+    it('returns 0 if it is the only number', () => {
+        const arr = [0];
+        const result = largestProduct(arr);
+        expect(result).toEqual(0);
+    });
+
+    it('returns thee number if its the only one', () => {
+        const arr = [14];
+        const result = largestProduct(arr);
+        expect(result).toEqual(arr[0]);
+    });
 });

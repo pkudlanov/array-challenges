623a299ebd99d3ffbb48be2849d2cfaf5aa326ba
Pavel Kudlanov <pkudlanov@yahoo.com>
2019-07-08 23:47:54 -0700

    test passes but definetly not correct test, have no idea how to correctly test

--- test/findMissingNum.test.js
+++ test/findMissingNum.test.js
@@ -6,4 +6,10 @@ describe('test findMissingNum function', () => {
         const result = findMissingNum(arr, 9, 1);
         expect(result).toEqual(expect.any(Number));
     });
+
+    it('sorts the passed array from smallest to largest', () => {
+        const arr = [2, 5, 1, 4, 9, 6, 3, 7];
+        const result = findMissingNum(arr, 9, 1);
+        expect(result).toEqual(1);
+    });
 });

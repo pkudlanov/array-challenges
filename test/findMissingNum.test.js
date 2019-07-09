881511cd5583c6f3dd9e1deb8649edd0d12e2cca
Pavel Kudlanov <pkudlanov@yahoo.com>
2019-07-09 00:05:15 -0700

    it passes all tests even though two are the same

--- test/findMissingNum.test.js
+++ test/findMissingNum.test.js
@@ -10,6 +10,12 @@ describe('test findMissingNum function', () => {
     it('sorts the passed array from smallest to largest', () => {
         const arr = [2, 5, 1, 4, 9, 6, 3, 7];
         const result = findMissingNum(arr, 9, 1);
-        expect(result).toEqual(1);
+        expect(result).toEqual(expect.any(Number));
+    });
+
+    it('returns the number thats missing', () => {
+        const arr = [2, 5, 1, 4, 9, 6, 3, 7];
+        const result = findMissingNum(arr, 9, 1);
+        expect(result).toEqual(8);
     });
 });

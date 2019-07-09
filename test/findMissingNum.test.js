6475ef7db651863d49586651d70c3d3f67d75a32
Pavel Kudlanov <pkudlanov@yahoo.com>
2019-07-08 23:34:37 -0700

    set up findMissinNum function and wrote first test

---
+++ test/findMissingNum.test.js
@@ -0,0 +1,9 @@
+const { findMissingNum } = require('../lib/findMissingNum');
+
+describe('test findMissingNum function', () => {
+    it('returns number', () => {
+        const arr = [2, 5, 1, 4, 9, 6, 3, 7];
+        const result = findMissingNum(arr, 9, 1);
+        expect(result).toEqual(expect.any(Number));
+    });
+});
